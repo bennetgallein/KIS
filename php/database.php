445571964a1751a4c818a530a2fb3f061eab8714
Bennet Gallein <bennet@intranetproject.net>
2018-01-13 19:30:20 +0100

    language system & database

--- php/database.php
+++ php/database.php
@@ -11,21 +11,47 @@ class DB {
     private $cfg;
     private $connection;
 
+    private $lang;
+
     public function __construct() {
         $json_file = dirname(__FILE__) . "/../config.json";
         $json = file_get_contents($json_file);
         $this->cfg = json_decode($json, true);
+
+        $lang_file = dirname(__FILE__) . "/../messages_" . $this->cfg['lang'] . ".json";
+        $json = file_get_contents($lang_file);
+        $this->lang = json_decode($json, true);
     }
 
-    function connect() {
+    private function connect() {
         $this->connection = $db = new MySQLi($this->cfg["database"][0]["host"], $this->cfg['database'][0]['user'], $this->cfg['database'][0]['password'], $this->cfg['database'][0]['database']);
     }
 
-    function disconnect() {
+    private function disconnect() {
         $this->connection->close();
     }
-    function prepareQuery($query, $params) {
+    /*
+     *
+     * $params = array(
+        "i" => $number,
+        "s" => $string,
+        "d" => $double
+     * );
+     *
+     */
+    public function prepareQuery($query, $params) {
+        $this->connect();
         $prep = $this->connection->prepare($query);
-        foreach($samplearr as $key => $item){
+        foreach ($params as $type => $item) {
+            $prep->bind_param($type, $item);
+        }
+        return $prep->execute();
+        $this->disconnect();
+    }
+    function getConfig() {
+        return $this->cfg;
+    }
+    function gM($code) {
+        return $this->lang[$code];
     }
 }
