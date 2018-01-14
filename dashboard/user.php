278b1da78090da82d32579133de1217a9a1aeb07
Tobi2K <tobika2000.tk@gmail.com>
2018-01-14 18:46:42 +0100

    user.php edited

--- dashboard/user.php
+++ dashboard/user.php
@@ -94,7 +94,7 @@ $db = new DB();
         <div class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-8">
+                    <div class="col-md-12">
                         <div class="card">
                             <div class="card-header" data-background-color="purple">
                                 <h4 class="title">Edit Profile</h4>
@@ -164,40 +164,9 @@ $db = new DB();
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label>About Me</label>
-                                                <div class="form-group label-floating">
-                                                    <label class="control-label"> Lamborghini Mercy, Your chick she so
-                                                        thirsty, I'm in that two seat Lambo.</label>
-                                                    <textarea class="form-control" rows="5"></textarea>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
                                     <button type="submit" class="btn btn-primary pull-right">Update Profile</button>
                                     <div class="clearfix"></div>
                                 </form>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card card-profile">
-                            <div class="card-avatar">
-                                <a href="#pablo">
-                                    <img class="img" src="../assets/img/faces/marc.jpg"/>
-                                </a>
-                            </div>
-                            <div class="content">
-                                <h6 class="category text-gray">CEO / Co-Founder</h6>
-                                <h4 class="card-title">Alec Thompson</h4>
-                                <p class="card-content">
-                                    Don't be scared of the truth because we need to restart the human foundation in
-                                    truth And I love you like Kanye loves Kanye I love Rick Owens’ bed design but the
-                                    back is...
-                                </p>
-                                <a href="#pablo" class="btn btn-primary btn-round">Follow</a>
                             </div>
                         </div>
                     </div>
@@ -208,34 +177,16 @@ $db = new DB();
             <div class="container-fluid">
                 <nav class="pull-left">
                     <ul>
-                        <li>
-                            <a href="#">
-                                Home
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Company
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Portfolio
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Blog
-                            </a>
-                        </li>
+                        <?php
+                        foreach ($db->getFooter()[0] as $name => $link) {
+                            echo '<li><a href="' . $link . '">' . $name . '</a></li>';
+                        }
+                        ?>
                     </ul>
                 </nav>
                 <p class="copyright pull-right">
-                    &copy;
-                    <script>
-                        document.write(new Date().getFullYear())
-                    </script>
-                    <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
+                    &copy;<?= "2017-" . date('Y') ?>
+                    <a href="https://www.intranetproject.net">IntranetProject</a>, made with love an passion
                 </p>
             </div>
         </footer>
