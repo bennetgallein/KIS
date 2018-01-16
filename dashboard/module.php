d8a009b87928fa6f6a0bab1955f250d3e0cb655f
Tobi2K <tobika2000.tk@gmail.com>
2018-01-16 23:16:33 +0100

    moved notifications in to separate file

--- dashboard/module.php
+++ dashboard/module.php
@@ -55,30 +55,7 @@ $amodule = $_GET['module'];
                 </div>
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="material-icons">notifications</i>
-                                <span class="notification">5</span>
-                                <p class="hidden-lg hidden-md">Notifications</p>
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="#">Mike John responded to your email</a>
-                                </li>
-                                <li>
-                                    <a href="#">You have 5 new tasks</a>
-                                </li>
-                                <li>
-                                    <a href="#">You're now friend with Andrew</a>
-                                </li>
-                                <li>
-                                    <a href="#">Another Notification</a>
-                                </li>
-                                <li>
-                                    <a href="#">Another One</a>
-                                </li>
-                            </ul>
-                        </li>
+                        <?php include "notifications.php" ?>
                         <li>
                             <a href="user.php">
                                 <i class="material-icons">person</i>
