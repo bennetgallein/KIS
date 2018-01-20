d24ed060cd734400e1349925b618b1626e41b2c5
Tobi2K <tobika2000.tk@gmail.com>
2018-01-20 19:54:45 +0100

    ticket, need to talk

---
+++ modules/ticket/index.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header" data-background-color="purple">
+                <h4 class="title">Ticket No. #</h4>
+                <p class="category">Topic</p>
+            </div>
+            <div class="card-content">
+                This will be the content of the ticket.
+            </div>
+        </div>
+    </div>
+</div>
