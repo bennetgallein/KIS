ee593866b8d472472cfe908e71949c05a7783054
Bennet Gallein <bennet@intranetproject.net>
2018-01-22 19:19:54 +0100

    customers can create tickets.

--- modules/support/createticket.php
+++ modules/support/createticket.php
@@ -1,5 +1,23 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (isset($_POST['title']) && isset($_POST['message']) && isset($_POST['product'])) {
 
+
+        // insert and notification to user
+        $userid = $user->getId();
+
+        $db->simpleQuery("INSERT INTO tickets (title, userid, product) VALUES ('" . $db->getConnection()->escape_string($_POST['title']) . "', '" . $db->getConnection()->escape_string($userid) . "', '" . $db->getConnection()->escape_string($_POST['product']) . "')");
+        $res = $db->getConnection()->query("SELECT LAST_INSERT_ID()");
+        $res = $res->fetch_assoc();
+        $db->prepareQuery("INSERT INTO tickets_messages (ticketid, message) VALUES (?, ?)", array(
+            $db->escape($res['LAST_INSERT_ID()']), $db->escape($_POST['message']))
+        );
+
+        echo "Your Ticket was submitted!";
+    } else {
+        echo "Please fill every field!";
+    }
+}
 ?>
 <div class="row">
     <div class="col-md-12">
@@ -7,36 +25,34 @@
             <div class="card-header" data-background-color="blue">
                 <h4 class="title">Create a Ticket</h4>
             </div>
-            <div class="card-content">
-                <div class="input-group col-md-12">
-                    <input type="text" class="form-control" placeholder="Title">
-                </div>
+            <form action="module.php?module=support/createticket.php" method="post">
+                <div class="card-content">
+                    <div class="input-group col-md-12">
+                        <input type="text" name="title" class="form-control" placeholder="Title">
+                    </div>
+                    <label>Product:
+                        <select name="product">
+                            <option value="123">Webhosting</option>
+                            <option value="231">vServer</option>
+                            <option value="321">KVM</option>
+                            <option value="132">PLEXwas</option>
+                        </select>
+                    </label>
 
-                <div class="dropdown">
-                    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"  data-background-color="blue">
-                        Product
-                        <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                        <li><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">4</a></li>
-                        <li><a href="#">5</a></li>
-                    </ul>
-                </div>
+                    <div class="input-group col-md-12">
+                        <textarea type="text" name="message" class="form-control" placeholder="Ticket text" id="comment"
+                                  rows="5"></textarea>
+                    </div>
 
-                <div class="input-group col-md-12">
-                    <textarea type="text" class="form-control" placeholder="Ticket text" id="comment" rows="5"></textarea>
-                </div>
-
-                <div class="dropdown">
-                    <button class="btn btn-default dropdown-toggle pull-right" type="button" data-background-color="blue">
-                        Submit
-                        <i class="material-icons">send</i>
-                    </button>
-                </div>
-            </div>
+                    <div class="dropdown">
+                        <button class="btn btn-default dropdown-toggle pull-right" type="submit"
+                                data-background-color="blue">
+                            Submit
+                            <i class="material-icons">send</i>
+                        </button>
+                    </div>
+            </form>
         </div>
     </div>
+</div>
 </div>
