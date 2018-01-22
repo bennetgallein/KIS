8ac7cdc0e6e092a647854a655b659dee46922fa4
Tobi2K <tobika2000.tk@gmail.com>
2018-01-22 17:40:31 +0100

    ticket submit page

--- modules/support/createticket.php
+++ modules/support/createticket.php
@@ -1,3 +1,40 @@
-<?php
-echo "TEst";
-?>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header" data-background-color="blue">
+                <h4 class="title">Create a Ticket</h4>
+            </div>
+            <div class="card-content">
+                <div class="input-group col-md-12">
+                    <input type="text" class="form-control" placeholder="Title">
+                </div>
+
+                <div class="dropdown">
+                    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"  data-background-color="blue">
+                        Product
+                        <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                    </ul>
+                </div>
+
+                <div class="input-group col-md-12">
+                    <textarea type="text" class="form-control" placeholder="Ticket text" id="comment" rows="5"></textarea>
+                </div>
+
+                <div class="dropdown">
+                    <button class="btn btn-default dropdown-toggle pull-right" type="button" data-background-color="blue">
+                        Submit
+                        <i class="material-icons">send</i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
