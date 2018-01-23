e303c3b672160af675378eb4fa94ebad7958343f
Bennet Gallein <bennet@intranetproject.net>
2018-01-23 19:47:31 +0100

    lemme push this:
    - anwsering to support tickets
    - closing tickets
    
    TODO:
    
    - ability to take charge over the a ticket
    - closing tickets as supporter / admin
    - NOT respond to a ticket if it's closed!

--- modules/support/ticket.php
+++ modules/support/ticket.php
@@ -7,6 +7,30 @@ $id = $params->id;
 
 $res = $db->simpleQuery("SELECT * FROM tickets WHERE id=" . $db->getConnection()->escape_string($id) . " LIMIT 1");
 $row = $res->fetch_object();
+if (!($res->num_rows == 1)) {
+    die("NO TICKET WITH THAT ID FOUND");
+}
+
+if (property_exists($params, 'awnser')) {
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        if (isset($_POST['message'])) {
+            if ($row->userid == $user->getId()) {
+                $awns = 2;
+            } else {
+                $awns = 1;
+            }
+            $db->simpleQuery("INSERT INTO tickets_messages (ticketid, message, awnser) VALUES ('" . $row->id . "', '" . $db->getConnection()->escape_string($_POST['message']) . "', '" . $awns . "')");
+        }
+    }
+}
+if (property_exists($params, 'close')) {
+    if ($row->userid == $user->getId()) {
+        $db->simpleQuery("UPDATE tickets SET status = 2 WHERE id='" . $id . "'");
+        $db->simpleQuery("INSERT INTO tickets_messages (ticketid, message, awnser) VALUES ('" . $row->id . "', 'Ticket closed by customer', 3)");
+    } else {
+        die("YOU DON'T HAVE PERMISSION!");
+    }
+}
 ?>
 <div class="row">
     <div class="col-md-12">
@@ -21,28 +45,29 @@ $row = $res->fetch_object();
                         <div class="panel-body">
                             <?php
                             $res = $db->simpleQuery("SELECT * FROM tickets_messages WHERE ticketid=" . $db->getConnection()->escape_string($id));
-                            while ($row = $res->fetch_object()) {
-                                if ($row->awnser == '1'):?>
+                            while ($row1 = $res->fetch_object()) {
+                                if ($row1->awnser == '1'):?>
                                     <div class="panel-group col-md-10 pull-right">
                                         <div class="panel panel-default">
                                             <div class="panel-body" data-background-color="blue">
-                                                <?= $row->message ?>
+                                                <?= $row1->message ?>
                                             </div>
                                         </div>
                                     </div>
-                                <?php elseif ($row->awnser == '2'): ?>
+                                <?php elseif ($row1->awnser == '2'): ?>
                                     <div class="panel-group col-md-10">
                                         <div class="panel panel-default">
                                             <div class="panel-body">
-                                                <?= $row->message ?>
+                                                <?= $row1->message ?>
                                             </div>
                                         </div>
                                     </div>
                                 <?php else: ?>
                                     <div class="panel-group col-md-12">
                                         <div class="panel panel-default">
-                                            <div class="panel-body" data-background-color="red" style="text-align: center;">
-                                                <?= $row->message ?>
+                                            <div class="panel-body" data-background-color="red"
+                                                 style="text-align: center;">
+                                                <?= $row1->message ?>
                                             </div>
                                         </div>
                                     </div>
@@ -54,17 +79,23 @@ $row = $res->fetch_object();
                 </div>
             </div>
             <div class="panel" style="background-color: #EDEDED">
-                <div class="input-group col-md-12">
+                <form action="module.php?module=support/ticket.php&params=id|<?= $id ?>_awnser|1" method="post">
+                    <div class="input-group col-md-12">
                         <textarea type="text" name="message" class="form-control" placeholder="Response" id="comment"
                                   rows="5"></textarea>
-                </div>
-                <button class="btn btn-default col-md-3" data-background-color="red"><i
-                            class="material-icons">close</i>Close ticket
-                </button>
-                <div class="dropdown">
-                    <button class="btn btn-default pull-right" type="submit" data-background-color="blue">Submit<i
-                                class="material-icons">send</i></button>
-                </div>
+                    </div>
+                    <?php if ($row->status != 2): ?>
+                    <button class="btn btn-default col-md-3" data-background-color="red"><i
+                                class="material-icons">close</i><a
+                                href="module.php?module=support/ticket.php&params=id|<?= $id ?>_close|1">Close
+                            ticket</a>
+                    </button>
+                    <?php endif; ?>
+                    <div class="dropdown">
+                        <button class="btn btn-default pull-right" type="submit" data-background-color="blue">Submit<i
+                                    class="material-icons">send</i></button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
