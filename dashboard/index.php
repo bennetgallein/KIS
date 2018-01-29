d12852ad90bc0307cc73a5a5ba74b5558872f4b4
Bennet Gallein <bennet@intranetproject.net>
2018-01-29 22:18:32 +0100

    moved trello bug report list to another board.

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * clientID: AYqL3LlsQuoOAj14xojHy-rlcYExCkO9oUUOWZu8TpjTjiD75aiGaBvS6R154qrvISPN3jTpOpb7WoSJ
+ * secret: EIIlvkfgiROn3kYslXOT-VcuBmn_qk33ZF8VaSQNzEgKkGzmReQIKM0Hr0qzmffN35vglZC51-C4Ju7Q
+ */
 include("../php/database.php");
 include(dirname(__FILE__) . "/../php/User.php");
 
