65487c664ade80e0288113d22b2b6ec9ecf32b32
Bennet Gallein <bennet@intranetproject.net>
2018-01-30 17:28:02 +0100

    reset half the stuff

---
+++ modules/modules/module_balance/balance/manager.php
@@ -0,0 +1,32 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header" data-background-color="<?= $db->getConfig()['color']?>">
+                <h4 class="title">Transactions</h4>
+                <p class="category">These are your recent transactions.</p>
+            </div>
+            <div class="card-content table-responsive">
+                <table class="table">
+                    <thead class="text-primary">
+                    <tr>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Bought on</th>
+                        <th>Dispatched to:</th>
+                        <th>Order ID</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>23$</td>
+                        <td>28-01-2018</td>
+                        <td>Town</td>
+                        <td>325C36E5345A</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
