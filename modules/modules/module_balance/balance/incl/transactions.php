183ee94185a96c465e52b7fecd40e764e44f9a51
Tobi2K <tobika2000.tk@gmail.com>
2018-01-31 16:41:49 +0100

    transactions.php, don't understand why tho

---
+++ modules/modules/module_balance/balance/incl/transactions.php
@@ -0,0 +1,28 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header" data-background-color="<?= $db->getConfig()['color']?>">
+                <h4 class="title">Monthly transactions</h4>
+                <p class="category">These are your transactions this month.</p>
+            </div>
+            <div class="card-content table-responsive">
+                <table class="table">
+                    <thead class="text-primary">
+                    <tr>
+                        <th>Order ID</th>
+                        <th>Price</th>
+                        <th>Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>325C36E5345A</td>
+                        <td>23$</td>
+                        <td>28-01-2018</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
