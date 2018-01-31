6a2359b90a29790169a6f7f3d20878dea0829848
Bennet Gallein <bennet@intranetproject.net>
2018-01-31 23:19:55 +0100

    some brainfuck things just happened

--- modules/modules/module_balance/balance/incl/transactions.php
+++ modules/modules/module_balance/balance/incl/transactions.php
@@ -7,7 +7,7 @@ if (!$query) {
 <div class="row">
     <div class="col-md-12">
         <div class="card">
-            <div class="card-header" data-background-color="<?= $db->getConfig()['color']?>">
+            <div class="card-header" data-background-color="<?= $db->getConfig()['color'] ?>">
                 <h4 class="title">Monthly transactions</h4>
                 <p class="category">These are your transactions in the last month.</p>
             </div>
@@ -17,6 +17,7 @@ if (!$query) {
                     <tr>
                         <th>Order ID</th>
                         <th>Price</th>
+                        <th>Message</th>
                         <th>Date</th>
                     </tr>
                     </thead>
@@ -25,13 +26,15 @@ if (!$query) {
                     if ($query->num_rows == 0) {
                         echo "No Results found!";
                     } else {
-                    while ($row = $query->fetch_object()): ?>
-                    <tr>
-                        <td><?= $row->id ?></td>
-                        <td><?= $row->price ?>€</td>
-                        <td><?= $row->createdate ?></td>
-                    </tr>
-                    <?php endwhile; } ?>
+                        while ($row = $query->fetch_object()): ?>
+                            <tr>
+                                <td><?= $row->id ?></td>
+                                <td><?= (($row->positive) ? "+" : "-") . $row->price ?>€</td>
+                                <td><?= $row->text ?></td>
+                                <td><?= $row->createdate ?></td>
+                            </tr>
+                        <?php endwhile;
+                    } ?>
                     </tbody>
                 </table>
             </div>
