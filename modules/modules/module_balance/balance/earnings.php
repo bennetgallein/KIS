3c52dd91b604af86df82d92b1a3c909695ed4cf9
Tobi2K <tobika2000.tk@gmail.com>
2018-01-31 23:31:49 +0100

    earnings overview

---
+++ modules/modules/module_balance/balance/earnings.php
@@ -0,0 +1,66 @@
+<div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-header" data-background-color="green">
+                <i class="material-icons">attach_money</i>
+            </div>
+            <div class="card-content">
+                <p class="category">Earned:</p>
+                <h3 class="title">308976</h3>
+            </div>
+            <div class="card-footer">
+                <div class="stats">
+                    <i class="material-icons">date_range</i> last Day
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-header" data-background-color="orange">
+                <i class="material-icons">attach_money</i>
+            </div>
+            <div class="card-content">
+                <p class="category">Earned:</p>
+                <h3 class="title">308976</h3>
+            </div>
+            <div class="card-footer">
+                <div class="stats">
+                    <i class="material-icons">date_range</i> last 7 Days
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-header" data-background-color="red">
+                <i class="material-icons">attach_money</i>
+            </div>
+            <div class="card-content">
+                <p class="category">Earned:</p>
+                <h3 class="title">308976</h3>
+            </div>
+            <div class="card-footer">
+                <div class="stats">
+                    <i class="material-icons">date_range</i> last 30 Days
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card card-stats">
+            <div class="card-header" data-background-color="grey">
+                <i class="material-icons">attach_money</i>
+            </div>
+            <div class="card-content">
+                <p class="category">Earned:</p>
+                <h3 class="title">308976</h3>
+            </div>
+            <div class="card-footer">
+                <div class="stats">
+                    <i class="material-icons">date_range</i> Overall
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
