8ba165c9fa56add412b230c8c53becdd4cc40d0c
Bennet Gallein <bennet@intranetproject.net>
2018-01-31 22:47:10 +0100

    random work

--- modules/modules/module_balance/balance/manager.php
+++ modules/modules/module_balance/balance/manager.php
@@ -1,47 +1,39 @@
+<?php
+$query = $db->simpleQuery("SELECT * FROM balance_transactions WHERE userid='" . $user->getId() . "'");
+if (!$query) {
+    die("MySQL ERROR! <a href='changelog.php'>Submit Error here!</a>");
+}
+?>
 <div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-header" data-background-color="<?= $db->getConfig()['color']?>">
-                <h4 class="title">Transactions</h4>
-                <p class="category">These are your recent transactions.</p>
+                <h4 class="title">Monthly transactions</h4>
+                <p class="category">These are your transactions of all time.</p>
             </div>
             <div class="card-content table-responsive">
                 <table class="table">
                     <thead class="text-primary">
                     <tr>
-                        <th>Product</th>
-                        <th>Price</th>
-                        <th>Bought on</th>
-                        <th>Dispatched to:</th>
                         <th>Order ID</th>
+                        <th>Price</th>
+                        <th>Date</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>23$</td>
-                        <td>28-01-2018</td>
-                        <td>Town</td>
-                        <td>325C36E5345A</td>
-                    </tr>
+                    <?php
+                    if ($query->num_rows == 0) {
+                        echo "No Results found!";
+                    } else {
+                        while ($row = $query->fetch_object()): ?>
+                            <tr>
+                                <td><?= $row->id ?></td>
+                                <td><?= $row->price ?>€</td>
+                                <td><?= $row->createdate ?></td>
+                            </tr>
+                        <?php endwhile; } ?>
                     </tbody>
                 </table>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="col-md-6">
-    <div class="card">
-        <div class="card-header" data-background-color="orange">
-            <?= "100€" ?>
-        </div>
-        <div class="card-content">
-            <h4 class="title">Daily Sales</h4>
-            <p class="category"><span class="text-success"><i class="fa fa-long-arrow-up"></i> 55%  </span> increase in today sales.</p>
-        </div>
-        <div class="card-footer">
-            <div class="stats">
-                <i class="material-icons">access_time</i> updated 4 minutes ago
             </div>
         </div>
     </div>
