809dcecacd5bd26b87e388598bc3bf3f8f135044
Tobi2K <tobika2000.tk@gmail.com>
2018-02-01 11:06:21 +0100

    simple error page

---
+++ error.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+    <meta charset="utf-8"/>
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/favicon.png"/>
+    <link rel="icon" type="image/png" href="assets/favicon.png"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Confirmation</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
+    <meta charset="utf-8"/>
+    <!-- Bootstrap core CSS     -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+    <!--  Material Dashboard CSS    -->
+    <link href="assets/css/material-dashboard.css?v=1.2.0" rel="stylesheet"/>
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="assets/css/demo.css" rel="stylesheet"/>
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet'
+          type='text/css'>
+    <link href="assets/css/style.css" rel="stylesheet"/>
+</head>
+
+<body>
+<div class="container" style="margin-top: 15%;">
+    <h2>It seems like you requested a site that wasn't found or there has been an internal error.<br><br>Either way, if this
+        error persists you can contact us. <br>Email: <a href="mailto:support@fee-hosting.de">support@fee-hosting.de</a>
+        <br> Tweet us: <a href="https://twitter.com/FeeHostingcom">Twitter</a>.</h2>
+</div>
+</body>
+
+</html>
