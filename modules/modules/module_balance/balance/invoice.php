d43949dc1da1a93483b49d1f49da04e759910ab3
Tobi2K <tobika2000.tk@gmail.com>
2018-02-02 15:39:48 +0100

    invoice

---
+++ modules/modules/module_balance/balance/invoice.php
@@ -0,0 +1,66 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header" data-background-color="<?= $db->getConfig()['color'] ?>">
+                <h4 class="title">Invoice N° 236245</h4>
+            </div>
+            <div class="card-content table-responsive">
+                <div>
+                <div class="pull-right col-md-3">
+                    <p>Address</p>
+                    <p>City, ZIP</p>
+                    <p>Phone??</p>
+                    <h5>Date</h5>
+                </div>
+                <div class="pull-left">
+                    <h3>Billed to</h3>
+                    <p>Name/Company</p>
+                    <p>Address</p>
+                    <p>City, ZIP</p>
+                    <p>Phone??</p>
+                </div>
+                </div>
+                <table class="table">
+                    <thead class="text-primary">
+                    <tr>
+                        <th>Product</th>
+                        <th>Amount</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Server</td>
+                        <td>8</td>
+                    </tr>
+                    </tbody>
+                </table>
+                <div class="table-responsive pull-right col-md-3">
+                    <table class="table">
+                        <tr>
+                            <td>Subtotal</td>
+                            <td>50.00€</td>
+                        </tr>
+                        <tr>
+                            <td>Tax</td>
+                            <td>10.00%</td>
+                        </tr>
+                        <tr>
+                            <td>Other</td>
+                            <td>1.00€</td>
+                        </tr>
+                        <tr>
+                            <td>Total</td>
+                            <td>56.00€</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-md-12" style="font-size: 1.5em;">
+                    <p>If you experience any problems contact us here:</p><br>
+                    <h4>email</h4>
+                </div>
+                <br><br><br>
+                <h2 class="col-md-12">Thank you for your business!</h2>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ modules/modules/module_balance/balance/invoicelist.php
@@ -0,0 +1,25 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header" data-background-color="<?= $db->getConfig()['color']?>">
+                <h4 class="title">Invoice listing</h4>
+            </div>
+            <div class="card-content table-responsive">
+                <table class="table">
+                    <thead class="text-primary">
+                    <tr>
+                        <th>Order id</th>
+                        <th>Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><a href="#">123456</a></td>
+                            <td>02.02.2018</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
