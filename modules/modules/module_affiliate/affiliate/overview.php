256d57fb8371f3ce2a7db215cf6c6e1578f5c2b6
Tobi2K <tobika2000.tk@gmail.com>
2018-02-03 22:35:32 +0100

    affiliate

---
+++ modules/modules/module_affiliate/affiliate/overview.php
@@ -0,0 +1,21 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header" data-background-color="<?= $db->getConfig()['color'] ?>">
+                <h4 class="title">Become an affiliate</h4>
+            </div>
+            <div class="card-content">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3">
+                        <h4>Your affiliate link is:</h4><h3><a href="http://invite.kis.de">http://invite.kis.de</a></h3><h4>Copy it and invite your friends via it and get cool perks!</h4>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                                60%
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
