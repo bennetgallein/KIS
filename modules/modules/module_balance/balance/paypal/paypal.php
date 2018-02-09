529142724b0651cd14b2c2dd614577022387a93d
Bennet Gallein <bennet@intranetproject.net>
2018-02-09 18:35:57 +0100

    at least it's working..

--- modules/modules/module_balance/balance/paypal/paypal.php
+++ modules/modules/module_balance/balance/paypal/paypal.php
@@ -10,8 +10,7 @@ use PayPal\Api\RedirectUrls;
 use PayPal\Api\ItemList;
 use PayPal\Api\Item;
 
-include("vendor/autoload.php");
-
+include("../vendor/autoload.php");
 if (isset($_POST['amount'])) {
     $api = new ApiContext(
         new OAuthTokenCredential(
@@ -73,34 +72,5 @@ if (isset($_POST['amount'])) {
             $redirectUrl = $link->getHref();
         }
     }
-    $db->redirect($redirectUrl);
+    header('Location:' . $redirectUrl);
 }
-?>
-<div class="row">
-    <div class="col-md-4 col-md-offset-4">
-        <div class="card">
-            <div class="card-header" data-background-color="<?= $db->getConfig()['color'] ?>">
-                <h4 class="title">Add balance</h4>
-            </div>
-            <div class="card-content">
-                <form action="module.php?module=balance/add.php" method="post">
-                    <div class="row">
-                        <div class="col-md-6 col-md-offset-3">
-                            <div class="form-group label-floating">
-                                <label class="control-label">Amount</label>
-                                <input name="amount" value="" type="text" class="form-control">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-md-offset-3 text-center">
-                            <button type="submit" class="btn btn-default"
-                                    data-background-color="<?= $db->getConfig()['color'] ?>"><i
-                                        class="fab fa-paypal fa-lg"></i> Paypal
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
