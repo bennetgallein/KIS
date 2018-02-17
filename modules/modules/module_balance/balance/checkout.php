ffcfdd4d3199f312fc6cc7eae5b201178947b381
Tobi2K <tobika2000.tk@gmail.com>
2018-02-17 02:15:35 +0100

    hope it is like you wanted

---
+++ modules/modules/module_balance/balance/checkout.php
@@ -0,0 +1,84 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header" data-background-color="<?= $db->getConfig()['color'] ?>">
+                <h4 class="title">Checkout</h4>
+            </div>
+            <div class="card-content">
+                <div class="col-md-10">
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="col-md-2">
+                                <img src="http://lorempixel.com/400/400/sports/1/"
+                                     class="img-responsive img-circle"
+                                     alt="Responsive image">
+                            </div>
+                            <div class="col-md-10">
+                                <table class="table">
+                                    <tr>
+                                        <td>Category 1</td>
+                                        <td>Info 1</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Category 2</td>
+                                        <td>Info 2</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>Category 3</td>
+                                        <td>Info 3</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>Category 4</td>
+                                        <td>Info 4</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="card">
+                        <div class="card-content">
+                            <div class="col-md-2">
+                                <img src="http://lorempixel.com/400/400/sports/2/"
+                                     class="img-responsive img-circle"
+                                     alt="Responsive image">
+                            </div>
+                            <div class="col-md-10">
+                                <table class="table">
+                                    <tr>
+                                        <td>Category 1</td>
+                                        <td>Info 1</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Category 2</td>
+                                        <td>Info 2</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>Category 3</td>
+                                        <td>Info 3</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>Category 4</td>
+                                        <td>Info 4</td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="card-content text-center">
+                        <h4>Total:</h4>
+                        <h3>55.00$</h3>
+                        <hr>
+                        <button type="submit" class="btn" data-background-color="blue">Checkout</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
