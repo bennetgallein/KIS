aea80204856caa173fb0f58740abe62dea803cb3
bennetgallein <info@team-scorpia.de>
2018-02-19 22:28:06 +0100

    quite unsure

--- modules/modules/module_balance/balance/checkout.php
+++ modules/modules/module_balance/balance/checkout.php
@@ -1,3 +1,11 @@
+<?php
+if (!($_SERVER['REQUEST_METHOD'] != 'POST')) {
+    die("Not allowed");
+}
+$price = 13.2;
+$_POST = array("test" => "value1", "test2" => "value2");
+
+?>
 <div class="row">
     <div class="col-md-12">
         <div class="card">
@@ -11,67 +19,28 @@
                             <div class="col-md-12">
                                 <h3>Product name</h3>
                                 <table class="table">
-                                    <tr>
-                                        <td>Category 1</td>
-                                        <td>Info 1</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Category 2</td>
-                                        <td>Info 2</td>
-                                    </tr>
-
-                                    <tr>
-                                        <td>Category 3</td>
-                                        <td>Info 3</td>
-                                    </tr>
-
-                                    <tr>
-                                        <td>Category 4</td>
-                                        <td>Info 4</td>
-                                    </tr>
+                                    <?php
+                                    foreach ($_POST as $categorie => $detail): ?>
+                                        <tr>
+                                            <td><?= $categorie ?></td>
+                                            <td><?= $detail ?></td>
+                                        </tr>
+                                    <?php
+                                    endforeach;
+                                    ?>
                                 </table>
                                 <div class="col-md-2 pull-right">
-                                    <h4><u>Price:</u></h4><h5><b>22.5$</b></h5>
+                                    <h4><u>Price:</u></h4><h5><b><?= $price ?>€</b></h5>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <hr>
-                    <div class="card">
-                        <div class="card-content">
-                            <div class="col-md-12">
-                                <h3>Product name</h3>
-                                <table class="table">
-                                    <tr>
-                                        <td>Category 1</td>
-                                        <td>Info 1</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Category 2</td>
-                                        <td>Info 2</td>
-                                    </tr>
-
-                                    <tr>
-                                        <td>Category 3</td>
-                                        <td>Info 3</td>
-                                    </tr>
-
-                                    <tr>
-                                        <td>Category 4</td>
-                                        <td>Info 4</td>
-                                    </tr>
-                                </table>
-                                <div class="col-md-2 pull-right">
-                                    <h4><u>Price:</u></h4><h5><b>22.5$</b></h5>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
                 <div class="col-md-2">
                     <div class="card-content text-center">
                         <h4>Total:</h4>
-                        <h3>55.00$</h3>
+                        <h3><?= $price ?>€</h3>
                         <hr>
                         <button type="submit" class="btn" data-background-color="blue">Checkout</button>
                     </div>
