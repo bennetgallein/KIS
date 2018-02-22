d67068abfdceeffd87325c8085501c9ea40e56a9
bennetgallein <info@team-scorpia.de>
2018-02-22 23:28:22 +0100

    some playing with the github api

--- modules/modules/module_modulemanager/modulemanager/install.php
+++ modules/modules/module_modulemanager/modulemanager/install.php
@@ -1,2 +1,12 @@
 <?php
 // install modules from a GitHub repository link should be made easy from here
+
+include("vendor/autoload.php");
+
+$string = "bennetgallein/modulemanager";
+
+$string = explode("/", $string);
+
+$client = new \Github\Client();
+$readme = $client->api('repo')->contents()->readme('bennetgallein', 'PaladinsPHP', $client);
+var_dump($readme);
