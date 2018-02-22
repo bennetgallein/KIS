3859b166b3a008723e0839b9fe8781801e0e99dc
Tobi2K <tobika2000.tk@gmail.com>
2018-02-22 12:39:46 +0100

    dropdown made nice

--- dashboard/notifications.php
+++ dashboard/notifications.php
@@ -2,6 +2,16 @@
 
 $res = $db->simpleQuery("SELECT * FROM notifications WHERE userid='" . $user->getId() . "' AND isread = 0 ORDER BY inserted DESC");
 ?>
+<style>
+    .dropdown-menu li a:hover, .dropdown-menu li a:focus, .dropdown-menu li a:active {
+        background-color: #288FEB !important;
+    }
+    .dropdown-menu .lidrop a:hover, .dropdown-menu .lidrop a:focus, .dropdown-menu .lidrop a:active {
+        background-color: #FFFFFF !important;
+        color: #000 !important;
+        box-shadow: none !important;
+    }
+</style>
 <li class="dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
         <i class="material-icons">notifications</i>
@@ -17,7 +27,7 @@ $res = $db->simpleQuery("SELECT * FROM notifications WHERE userid='" . $user->ge
     <ul class="dropdown-menu">
         <?php
         if ($res->num_rows == 0)
-            echo '<li><a>No new notifications!</a></li>';
+            echo '<li class="lidrop"><a>No new notifications!</a></li>';
 
         while ($row = $res->fetch_object()) {
             echo '<li><a href="index.php?removenotification=1&id=' . $row->id . '">' . $row->message . ' <i class="material-icons" style="cursor: pointer">highlight_off</i></a></li>';
