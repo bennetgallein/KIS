e3dc757ab8501df2bc8888a4e762acce36ac1233
Tobi2K <tobika2000.tk@gmail.com>
2018-02-23 20:59:33 +0100

    product page

---
+++ modules/modules/module_productmanager/productmanager/product.php
@@ -0,0 +1,117 @@
+<div class="row">
+    <div class="col-md-4">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="card">
+                    <div class="card-header" data-background-color="<?= $db->getConfig()['color'] ?>">
+                        <h4 class="title text-center">Product name</h4>
+                    </div>
+                    <div class="card-content" style="text-align: center">
+                        <h5><span style="border-bottom: 1px solid #000;">Erhältlich ab:</span></h5>
+                        <h2><b>4,49$</b>/mo</h2>
+                        <table class="table text-center">
+                            <tr>
+                                <td>1 vCore</td>
+                            </tr>
+                            <tr>
+                                <td>512MB RAM</td>
+                            </tr>
+                            <tr>
+                                <td>10 GB SSD</td>
+                            </tr>
+                            <tr>
+                                <td>Dauerschutz DDOS Protection</td>
+                            </tr>
+                            <tr>
+                                <td>250 Mbit/s Anbindung</td>
+                            </tr>
+                            <tr>
+                                <td>Fair Use Traffic</td>
+                            </tr>
+                            <tr>
+                                <td>KVM virtualisiert</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="card">
+                    <div class="card-header" data-background-color="<?= $db->getConfig()['color'] ?>">
+                        <h4 class="title text-center">Product name</h4>
+                    </div>
+                    <div class="card-content" style="text-align: center">
+                        <h5><span style="border-bottom: 1px solid #000;">Erhältlich ab:</span></h5>
+                        <h2><b>4,49$</b>/mo</h2>
+                        <table class="table text-center">
+                            <tr>
+                                <td>1 vCore</td>
+                            </tr>
+                            <tr>
+                                <td>512MB RAM</td>
+                            </tr>
+                            <tr>
+                                <td>10 GB SSD</td>
+                            </tr>
+                            <tr>
+                                <td>Dauerschutz DDOS Protection</td>
+                            </tr>
+                            <tr>
+                                <td>250 Mbit/s Anbindung</td>
+                            </tr>
+                            <tr>
+                                <td>Fair Use Traffic</td>
+                            </tr>
+                            <tr>
+                                <td>KVM virtualisiert</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="card">
+                    <div class="card-header" data-background-color="<?= $db->getConfig()['color'] ?>">
+                        <h4 class="title text-center">Product name</h4>
+                    </div>
+                    <div class="card-content" style="text-align: center">
+                        <h5><span style="border-bottom: 1px solid #000;">Erhältlich ab:</span></h5>
+                        <h2><b>4,49$</b>/mo</h2>
+                        <table class="table text-center">
+                            <tr>
+                                <td>1 vCore</td>
+                            </tr>
+                            <tr>
+                                <td>512MB RAM</td>
+                            </tr>
+                            <tr>
+                                <td>10 GB SSD</td>
+                            </tr>
+                            <tr>
+                                <td>Dauerschutz DDOS Protection</td>
+                            </tr>
+                            <tr>
+                                <td>250 Mbit/s Anbindung</td>
+                            </tr>
+                            <tr>
+                                <td>Fair Use Traffic</td>
+                            </tr>
+                            <tr>
+                                <td>KVM virtualisiert</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
