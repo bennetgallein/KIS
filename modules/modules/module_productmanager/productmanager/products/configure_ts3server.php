ecbbb82d35a0d47c271060d436d22a20190d67d4
Tobi2K <tobika2000.tk@gmail.com>
2018-02-25 20:38:13 +0100

    fml ugly af

---
+++ modules/modules/module_productmanager/productmanager/products/configure_ts3server.php
@@ -0,0 +1,182 @@
+<style>
+    .slidecontainer {
+        width: 100%;
+    }
+
+    .slider {
+        -webkit-appearance: none;
+        width: 100%;
+        height: 25px;
+        background: #d3d3d3;
+        outline: none;
+        opacity: 0.7;
+        -webkit-transition: .2s;
+        transition: opacity .2s;
+    }
+
+    .slider:hover {
+        opacity: 1;
+    }
+
+    .slider::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        appearance: none;
+        width: 25px;
+        height: 25px;
+        background: #4CAF50;
+        cursor: pointer;
+    }
+
+    .slider::-moz-range-thumb {
+        width: 25px;
+        height: 25px;
+        background: #4CAF50;
+        cursor: pointer;
+    }
+</style>
+<div class="row">
+    <div class="col-md-10 col-md-offset-1">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="card">
+                    <div class="card-header" data-background-color="<?= $db->getConfig()['color'] ?>">
+                        <h4 class="title text-center">Konfigurieren</h4>
+                    </div>
+                    <div class="card-content">
+                        <div class="col-md-9">
+                            <h3><b>Infos:</b></h3>
+                            <table class="table">
+                                <tr>
+                                    <td>Filetransfer:</td>
+                                    <td>Free</td>
+                                </tr>
+                                <tr>
+                                    <td>Setup:</td>
+                                    <td>Sofort</td>
+                                </tr>
+                                <tr>
+                                    <td>Anbindung:</td>
+                                    <td>bis zu 1Gbit</td>
+                                </tr>
+                                <tr>
+                                    <td>inklusive</td>
+                                    <td> DDos Protection</td>
+                                </tr>
+                                <tr>
+                                    <td>Unlimited</td>
+                                    <td>Traffic</td>
+                                </tr>
+
+                            </table>
+                            <form>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="sel1">Abrechnungszeitraum:</label>
+                                            <select class="form-control" id="sel1">
+                                                <option>#</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="slidecontainer">
+                                            <label for="myRange">Slots:</label>
+                                            <input type="range" min="10" max="350" value="10" class="slider" id="myRange">
+                                            <p>Current slots: <span id="demo"></span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="card-content text-center">
+                                <table class="table" style="font-size: 0.9em;">
+                                    <tr>
+                                        <td>Name</td>
+                                        <td>Price</td>
+                                    </tr>
+                                    <tr>
+                                        <td id="server">TS³-Server</td>
+                                        <td id="serverprice">0.00€</td>
+                                    </tr>
+                                    <tr>
+                                        <td id="slots">Slots: Anzahl</td>
+                                        <td id="slotsprice">0.00€</td>
+                                    </tr>
+                                    <tr>
+                                        <td id="ssd">SSD-Speicher: 10GB</td>
+                                        <td id="ssdprice">0.00€</td>
+                                    </tr>
+                                    <tr>
+                                        <td id="ips">IPs: 1 IP</td>
+                                        <td id="ipsprice">0.00€</td>
+                                    </tr>
+                                    <tr class="info">
+                                        <td>Monatlich</td>
+                                        <td>price</td>
+                                    </tr>
+                                </table>
+                                <h3>Zu bezahlen:</h3>
+                                <h4><b>
+                                        <div id="item-price">price</div>
+                                    </b></h4>
+                                <hr>
+                                <button type="submit" class="btn" data-background-color="blue">Checkout</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+<script>
+    $(function () {
+        $("select.calculate").on("change", calc);
+        $("select.calculate").on("change", update);
+        function calc() {
+            var basePrice = <?= $row->price ?>;
+            newPrice = basePrice;
+            $("select.calculate option:selected").each(function (idx, el) {
+                console.log($(el).data('price'));
+                newPrice += parseInt($(el).data('price'));
+                console.log(newPrice);
+            });
+
+            newPrice = newPrice.toFixed(2);
+            $("#item-price").html(newPrice + "€");
+
+        }
+        function update() {
+            $("select.ramsel option:selected").each(function (idx, el) {
+                $("#ramprice").html($(el).data('price') + "€");
+            });
+            $("select.cpusel option:selected").each(function (idx, el) {
+                $("#vcoresprice").html($(el).data('price') + "€");
+            });
+            $("select.ssdsel option:selected").each(function (idx, el) {
+                $("#ssdprice").html($(el).data('price') + "€");
+            });
+            $("select.ipsel option:selected").each(function (idx, el) {
+                $("#ipsprice").html($(el).data('price') + "€");
+            });
+        }
+    });
+
+
+</script>
+<script>
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+    }
+</script>
