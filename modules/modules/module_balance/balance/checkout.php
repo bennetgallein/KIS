bdccb341e5dd5352520d69ca7d3a7285a34a8030
Bennet Gallein <bennet@intranetproject.net>
2018-02-27 22:19:46 +0100

    enought for today.

--- modules/modules/module_balance/balance/checkout.php
+++ modules/modules/module_balance/balance/checkout.php
@@ -1,9 +1,8 @@
 <?php
-if (!($_SERVER['REQUEST_METHOD'] != 'POST')) {
+if (($_SERVER['REQUEST_METHOD'] != 'POST')) {
     die("Not allowed");
 }
-$price = 13.2;
-$_POST = array("test" => "value1", "test2" => "value2");
+//$_POST = array("name" => "Test Product", "test" => "value1", "return" => "test", "test2" => "value2", "total" => "13.2");
 
 ?>
 <div class="row">
@@ -17,10 +16,12 @@ $_POST = array("test" => "value1", "test2" => "value2");
                     <div class="card">
                         <div class="card-content">
                             <div class="col-md-12">
-                                <h3>Product name</h3>
+                                <h3><?= $_POST['name'] ?></h3>
+                                <?php unset($_POST['name']); ?>
                                 <table class="table">
                                     <?php
                                     foreach ($_POST as $categorie => $detail): ?>
+                                    <?php if ($categorie == "return" || $categorie == "total") continue; ?>
                                         <tr>
                                             <td><?= $categorie ?></td>
                                             <td><?= $detail ?></td>
@@ -30,7 +31,7 @@ $_POST = array("test" => "value1", "test2" => "value2");
                                     ?>
                                 </table>
                                 <div class="col-md-2 pull-right">
-                                    <h4><u>Price:</u></h4><h5><b><?= $price ?>€</b></h5>
+                                    <h4><u>Price:</u></h4><h5><b><?= $_POST['total'] ?>€</b></h5>
                                 </div>
                             </div>
                         </div>
@@ -40,7 +41,7 @@ $_POST = array("test" => "value1", "test2" => "value2");
                 <div class="col-md-2">
                     <div class="card-content text-center">
                         <h4>Total:</h4>
-                        <h3><?= $price ?>€</h3>
+                        <h3><?= $_POST['total'] ?>€</h3>
                         <hr>
                         <button type="submit" class="btn" data-background-color="blue">Checkout</button>
                     </div>
