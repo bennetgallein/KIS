0a038539d2152af411cc833aaca01326f0c04b13
Bennet Gallein <bennet@intranetproject.net>
2018-03-04 14:06:32 +0100

    ts3 orderedon and expectedrenewal

--- modules/modules/module_productmanager/productmanager/myproducts.php
+++ modules/modules/module_productmanager/productmanager/myproducts.php
@@ -7,6 +7,7 @@ if ($params->list != "all" || $params->list != "vserver" || $params->list != "ts
 }
 if ($params->list == "all") {
     $vservers = $db->simpleQuery("SELECT * FROM product_vserver WHERE userid='" . $user->getId() . "'");
+    $ts3server = $db->simpleQuery("SELECT * FROM product_ts3server WHERE userid='" . $user->getId() . "'");
 }
 ?>
 <div class="row">
@@ -43,6 +44,39 @@ if ($params->list == "all") {
                 </table>
             </div>
         </div>
+        <?php endif; ?>
+        <?php if (isset($ts3server)):?>
+            <div class="card">
+                <a href="#">
+                    <div class="card-header" data-background-color="<?= $db->getConfig()['color'] ?>">
+                        <h4 class="title">Product Overview</h4>
+                    </div>
+                </a>
+                <div class="card-content table-responsive">
+                    <table class="table">
+                        <thead class="text-primary">
+                        <tr>
+                            <th>ID</th>
+                            <th>Server ID</th>
+                            <th>Slots</th>
+                            <th>Bought on</th>
+                            <th>Expected Renewal</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php while ($row = $ts3server->fetch_object()):?>
+                            <tr>
+                                <td><?= $row->id ?></td>
+                                <td><?= $row->tsid?></td>
+                                <td><?= $row->slots ?></td>
+                                <td><?= $row->orderedon?></td>
+                                <td><?= $row->expectedrenewal?></td>
+                            </tr>
+                        <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         <?php endif; ?>
     </div>
 </div>
