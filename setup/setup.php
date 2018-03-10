69e9f20c8167ed0f8962a9f38246e1511ba37727
bennetgallein <bennet@intranetproject.net>
2018-03-11 00:41:41 +0100

    database setup

--- setup/setup.php
+++ setup/setup.php
@@ -1,20 +1,17 @@
 <?php
-/*
-1. checken in der config.json ob Werte für database host da sind.
-2. Lizenz prüfen (setup_database).
-3. Datenbank & Tabellen erstellen (setup_database).
-*/
+include("../php/database.php");
+$db = new DB();
+
 if (file_exists(dirname(__FILE__) . "/../config.json")) {
     $config = json_decode(file_get_contents(dirname(__FILE__) . "/../config.json"), true);
-    if ($config['database'][0]['host'] == ""):
+    if ($db->getConnection()->connect_errno == 1049):
         ?>
         <form action="setup_database.php" method="get">
             <input type="text" name="license" placeholder="License Key" />
-            <input type="submit" name="submit" />
+            <input type="submit"/>
         </form>
         <?php
     else:
         header("Location: ../index.php");
     endif;
 }
-?>
