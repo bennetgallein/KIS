63cfd4f2a7397ac040ada1d2c01c5d359b943752
bennetgallein <bennet@intranetproject.net>
2018-03-21 23:54:27 +0100

    removed modules

--- modules/modules/module_productmanager/productmanager/productoverview.php
+++
@@ -1,9 +0,0 @@
-<?php
-foreach ($module->getRaw()['products'] as $product) {
-    if ($product['active']) {
-        echo "<div class='row'><p class='navbar-brand'>";
-        echo $product['name'];
-        echo "</p></div>";
-        include($module->getPath() . "/" . $module->getBasePath() . $product['link']);
-    }
-}
