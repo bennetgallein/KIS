b43cacba16a8ed2775edfd4a92b0dedf89250b97
bennetgallein <bennet@intranetproject.net>
2018-03-23 01:49:19 +0100

    changed style of profile site. More will follow later this night

---
+++ bower_components/momentum/README.md
@@ -0,0 +1,10 @@
+# momentum
+momentum is an Admin Template that will fit your projects needs.
+
+### General information
+We are using a number of frameworks to make your and our lives easier. Here is a list of all of them:
+- [Material Design Icons](https://materialdesignicons.com/)
+- [Bootstrap v4.0.0](https://getbootstrap.com/)
+
+### Authors:
+Tobias Kalmbach and Bennet Gallein
--- dashboard/sidebar.php
+++ dashboard/sidebar.php
@@ -1,101 +1,80 @@
-<div class="sidebar" data-color="<?= $db->getConfig()['color'] ?>" data-image="../assets/img/sidebar-1.jpg">
-    <!--
-Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red"
-
-Tip 2: you can also add an image using data-image tag
--->
-    <div class="logo">
-        <a href="<?= $db->getConfig()['url'] ?>" class="simple-text">
-            <?= $db->getConfig()['site_name'] ?>
-        </a>
-    </div>
-    <div class="sidebar-wrapper">
+<div id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+            <a href="#" style="text-decoration: none;">
+                <a href="<?= $db->getConfig()['url']?>"><i class="mdi mdi-star"></i><?= $db->getConfig()['site_name'] ?>
+            </a>
+        </li>
         <?php
         $currentfile = basename($_SERVER["SCRIPT_FILENAME"]);
         ?>
-        <ul class="nav">
-            <?php
+            <li>
+                <?php
             $link = "index.php";
             $active = ($currentfile == $link) ? "active" : "";
             ?>
-            <li class="<?= $active ?>">
-                <a href="<?= $link ?>">
-                    <i class="material-icons">dashboard</i>
-                    <p>Dashboard</p>
-                </a>
+                    <a class="mdi mdi-home" href="#">Dashboard</a>
             </li>
             <?php
-            $link = "user.php";
-            $active = ($currentfile == $link) ? "active" : "";
-            ?>
-            <li class="<?= $active ?>">
-                <?php
-                $module = $db->getModuleByName("Balance Manager");
-                if ($module != false) {
-                    if ($module->isActive()) {
-                        if ($module->getIncludeable("display_money")['permission'] <= $user->getPermissions()) {
-                            include($module->getPath() . "/" . $module->getBasepath() . $module->getIncludeable("display_money")['link']);
-                            $money = getMoney($db, $user);
-                            $money = "<span class=\"badge\">" . $money . "€</span>";
-                        }
-                    }
-                }
-                ?>
-                <a href="<?= $link ?>">
-                    <i class="material-icons">person</i>
-                    <p><?= $db->m("sidebar_profile")?> <?= isset($money) ? $money : ""?></p>
-                </a>
-            </li>
-            <?php
-            unset($active);
-            foreach ($db->getModules() as $module) {
-                $hrset = false;
+            $module = $db->getModuleByName("Balance Manager");
+            if ($module != false) {
                 if ($module->isActive()) {
-                    foreach ($module->getNavs() as $navpoint) {
-                        if ($navpoint['type'] == 'nav') {
-                            if ($navpoint['permission'] <= $user->getPermissions()) {
-                                if ($hrset == false) {
-                                    echo "<hr>";
-                                }
-                                $hrset = true;
-                                $path = $module->getBasepath() . $navpoint['link'];
-                                if (isset($amodule)) {
-                                    if ($path == $amodule) {
-                                        $active = "active";
-                                    }
-                                }
-                                $active = isset($active) ? $active : "";
-                                echo '<li class="' . $active . '">
-                                      <a href="module.php?module=' . $module->getBasepath() . $navpoint['link'] . '">
-                                          <i class="material-icons">' . $navpoint['icon'] . '</i>
-                                          <p>' . $module->getMessage($navpoint['name']) . '</p>
-                                      </a>
-                                  </li>';
-                                $active = "";
-
-                            }
-                        }
+                    if ($module->getIncludeable("display_money")['permission'] <= $user->getPermissions()) {
+                        include($module->getPath() . "/" . $module->getBasepath() . $module->getIncludeable("display_money")['link']);
+                        $money = getMoney($db, $user);
+                        $money = "<span class=\"badge\">" . $money . "€</span>";
                     }
                 }
             }
-            unset($module);
             ?>
+            <li>
+                <a class="mdi mdi-person"><?= $db->m("sidebar_profile")?> <?= isset($money) ? $money : ""?></a>
+            </li>
+            <li>
+                <?php
+                unset($active);
+                foreach ($db->getModules() as $module) {
+                    if ($module->isActive()) {
+                        echo '<a class="collapseable-nav" data-toggle="collapse" href="#collapse" role="button" aria-expanded="false" aria-controls="collapseExample">Module<i class="mdi mdi-plus"></i></a>
+                        <div class="collapse" id="collapse">
+                            <ul class="collapsible">
+                        ';
+                        foreach ($module->getNavs() as $navpoint) {
+                            if ($navpoint['type'] == 'nav') {
+                                if ($navpoint['permission'] <= $user->getPermissions()) {
+                                    $path = $module->getBasepath() . $navpoint['link'];
+                                    if (isset($amodule)) {
+                                        if ($path == $amodule) {
+                                            $active = "active";
+                                        }
+                                    }
+                                    $active = isset($active) ? $active : "";
+                                    echo '<li class="' . $active . '">
+                                          <a href="module.php?module=' . $module->getBasepath() . $navpoint['link'] . '">
+
+                                              <p class="<mdi mdi-' . $navpoint['icon'] . '">' . $module->getMessage($navpoint['name']) . '</p>
+                                          </a>
+                                      </li>';
+                                    $active = "";
+
+                                }
+                            }
+                        }
+                        echo '</ul></div></li>';
+                    }
+                }
+                unset($module);
+                ?>
+            </li>
             <?php
             $link = "changelog.php";
             $active = ($currentfile == $link) ? "active" : "";
             ?>
             <li class="<?= $active ?>">
-                <a href="<?= $link ?>">
-                    <i class="material-icons">add_circle</i>
-                    <p><?= $db->m("sidebar_changelog") ?></p>
-                </a>
+                <a href="<?= $link ?>"><?= $db->m("sidebar_changelog")?></a>
             </li>
             <li>
-                <a href="../php/logout.php">
-                    <i class="material-icons">power_settings_new</i>
-                    <p><?= $db->m("sidebar_logout") ?></p>
-                </a>
+                <a href="../php/logout.php"><?= $db->m("sidebar_logout")?></a>
             </li>
-        </ul>
-    </div>
+    </ul>
 </div>
